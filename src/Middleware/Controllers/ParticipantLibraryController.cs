f3175ed7ee385a69b92edc12b1aa35820e3bf1ea
ruvsdev <ruvs@dev>
2016-07-27 20:26:46 +0100

    owin working?

--- src/Middleware/Controllers/ParticipantLibraryController.cs
+++ src/Middleware/Controllers/ParticipantLibraryController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 
-namespace Nexus.Participant.Library.Controllers
+namespace Nexus.Participant.Library.Middleware.Controllers
 {
     [RoutePrefix("participants")]
     public class ParticipantLibraryController : ApiController
